e197d9a0ee61315882c1ff34eb577bfaf1dd9db0
irsPROirs <35394214+irsPROirs@users.noreply.github.com>
2020-04-06 18:00:26 +0300

    lesson3

--- lesson3/5.py
+++ lesson3/5.py
@@ -1,7 +1,18 @@
-def my_func():
-    result
-    return result
+def my_func(my_str, result, f_fun):
+    for i in range(len(my_str)):
+        try:
+            result = result + int(my_str[i])
+        except:
+            f_fun = False
+            print('Введен стоп символ "/"')
+            break
+    print('Промежуточная сумма =', result)
+    return result, f_fun
 
 
-my_str = input('введите переменную x: ').split()
-print(my_str)
+result_sum = int()
+f = True
+while f:
+    my_str = input('введите числа через пробел или "/" для остановки: ').split()
+    result_sum, f = my_func(my_str, result_sum, f)
+print('Итог:', result_sum)
